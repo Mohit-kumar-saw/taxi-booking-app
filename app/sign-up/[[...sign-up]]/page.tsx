be448ceb107a3a26aa8a41ca8f533ab86a549a9d
Mohit kumar saw <115533020+Mohit-kumar-saw@users.noreply.github.com>
2024-05-28 18:08:15 +0530

    Update page.tsx

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,5 @@
 import { SignUp } from "@clerk/nextjs";
  
 export default function Page() {
-  return <div className=" h-[90vh] w-full flex justify-center items-center"> <SignUn /></div> 
+  return <div className=" h-[90vh] w-full flex justify-center items-center"> <SignUp /></div> 
 }
